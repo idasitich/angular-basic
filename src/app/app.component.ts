3dba88294ccde4437b26957238f1ebab8f1f416b
idasitich <karris.kk@gmail.com>
2023-02-24 12:39:50 +0700

    fisrt-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-basic';
+  newMemberName ="";
+  members: string[] =[];
+
+  onInput(members: string){
+    this.newMemberName=members;
+    console.log(this.newMemberName);
+  }
+  
+  addMember (){
+    this.members.push(this.newMemberName);
+    this.newMemberName="";
+    console.log(this.members);
+  }
 }
